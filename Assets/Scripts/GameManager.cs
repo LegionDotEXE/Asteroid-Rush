b39140155c806b5ac629ac3f84a943d956b1f877
Saurab S <ajay.v.v.743@gmail.com>
2026-06-03 17:33:17 -0700

    Added a 30s timer when Startime() is called

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,10 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
+    public float gameDuration = 30f;
+
     bool gameStarted = false;
+    float timeRemaining;
 
     void Awake()
     {
@@ -23,11 +26,26 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        
+        if (!gameStarted) return;
+
+        timeRemaining -= Time.deltaTime;
+        timeRemaining = Mathf.Max(timeRemaining, 0f);
+
+        Debug.Log("Time remaining: " + timeRemaining.ToString("F1"));
+
+        if (timeRemaining <= 0f)
+        {
+            gameStarted = false;
+            Debug.Log("Escaped.");
+        }
     }
 
     public void StartGame()
     {
+        timeRemaining = gameDuration;
         gameStarted = true;
     }
+
+    public float GetTimeRemaining() => timeRemaining;
+    public bool IsGameActive() => gameStarted;
 }
